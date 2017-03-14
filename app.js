2679e0a211f1520aa1540c7f26c8580c112dce13
Saul Greene <spdysg@gmail.com>
2017-03-14 12:04:10 -0700

    still adding user stories but crearted a constructor function and called it with my first project

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+'use strict'
 
-function project(title, description, date, contributors, link){
+
+function Project(title, description, date, contributors, link){
   this.title = title;
   this.description = description;
   this.date = date;
   this.contributors = contributors;
   this.link = link;
 }
+
+var Escape201 = new Project('Escape Code 201', '(PLACEHOLDER) This project was the final accumulation of our first JavaScript class in which we demonstrated all of our skills from the class within a browser game.', '3.3.17', 'Saul Greene, Michael Treat, Kevin Schmidt, Carlo Santos', 'https://michaeltreat.github.io/Escape-code-201/');
