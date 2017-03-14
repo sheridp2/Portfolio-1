4347d203f4d6d9c6c4e88ee7e591c44d5cda7314
Saul Greene <spdysg@gmail.com>
2017-03-14 11:52:30 -0700

    got my lintrc set up and my gitignore. Now working on user stories

---
+++ app.js
@@ -0,0 +1,8 @@
+
+function project(title, description, date, contributors, link){
+  this.title = title;
+  this.description = description;
+  this.date = date;
+  this.contributors = contributors;
+  this.link = link;
+}
