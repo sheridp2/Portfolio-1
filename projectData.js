62304ee3360aa54103f3f4e7a0ef372bfab37c02
Saul Greene <spdysg@gmail.com>
2017-03-16 12:35:29 -0700

    made the constructors append

---
+++ projectData.js
@@ -0,0 +1,27 @@
+'use strict'
+var projectData = [
+  {
+    title: 'Escape code 201',
+    img: 'blankimgURL',
+    description: 'blahblah',
+    date: 'blank1',
+    contributors: 'blank1',
+    link: 'blank1'
+  },
+  {
+    title: 'Blank2',
+    img: 'blankimgURL',
+    description: 'Blank2',
+    date: 'Blank2',
+    contributors: 'Blank2',
+    link: 'Blank2',
+  },
+  {
+    title: 'Blank3',
+    img: 'blankimgURL',
+    description: 'Blank3',
+    date: 'Blank3',
+    contributors: 'Blank3',
+    link: 'Blank3',
+  }
+]
